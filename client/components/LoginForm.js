e9f62170199c0048fbdeccef8bb4b7425c9769ab
ebuyco <buycoernie@gmail.com>
2019-06-03 12:30:30 +0800

    this props user events

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -13,9 +13,7 @@ class LoginForm extends Component {
   }
 
   componentWillUpdate(nextProps) {
-    // this.props // the old, current set of props
-    // nextProps // the next set of props that will be in place
-    // when the component rerenders
+    
     if (!this.props.data.user && nextProps.data.user) {
       // redirect to dashboard!!!!
       hashHistory.push('/dashboard');
